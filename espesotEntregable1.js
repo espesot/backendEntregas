91458a9e9e57d1bcee17329f42540f0ac96d5ee8
espesot <enzospesot@hotmail.com>
2022-12-03 15:14:14 -0300

    manejo de archivos

--- espesotEntregable1.js
+++ espesotEntregable1.js
@@ -1,9 +1,35 @@
+import fs from 'fs'
+
 class ProductManager {
-  constructor() {
+  constructor(path) {
     this.products = []
+    this.path = path
+  }
+
+  updateProduct(idProduct, newTitle, newDescripcion, newPrice, newThumbnail, newCode, newStock) {
+    this.#readProduct()
+    const foundedProduct = this.getProductByID(idProduct)
+    if (foundedProduct) {
+      this.deleteProduct(idProduct)
+      const updatedProduct = {
+        id: idProduct,
+        title: newTitle,
+        description: newDescripcion,
+        price: newPrice,
+        thumbnail: newThumbnail,
+        code: newCode,
+        stock: newStock
+      }
+      this.products.push(updatedProduct)
+      fs.writeFileSync(this.path,JSON.stringify(this.products),'utf-8')
+      console.log('El producto fue modificado')
+
+    }
+
   }
 
   addProduct = (title, description, price, thumbnail, code, stock) => {
+    this.#readProduct()
     if (title && description && price && thumbnail && code && stock) {
       if (this.products.find((product) => product.code === code)) {
         console.log('El codigo ya fue cargado')
@@ -20,6 +46,7 @@ class ProductManager {
         }
 
         this.products.push(product)
+        fs.writeFileSync(this.path, JSON.stringify(this.products),'utf-8')
         console.log('se agrego un producto')
       }
     } else {
@@ -28,18 +55,25 @@ class ProductManager {
   }
 
   getPruduct = () => {
+    this.#readProduct()
     return this.products
   }
 
-  getProductByID=(productID)=>{
+  getProductByID = (productID) => {
+    this.#readProduct()
     const pruductFunded = this.products.find((product) => product.id === productID)
-    if(pruductFunded){
+    if (pruductFunded) {
       return pruductFunded
-    }else{
+    } else {
       console.log('Producto no Encontrado')
     }
   }
 
+  deleteProduct(idProduct) {
+    this.#readProduct()
+    this.products = this.products.filter((prod) => prod.id !== idProduct)
+    fs.writeFileSync(this.path, JSON.stringify(this.products),'utf-8')
+  }
 
 
   #getMaxId = () => {
@@ -49,35 +83,29 @@ class ProductManager {
     })
     return maxId
   }
+
+  #readProduct(){
+    if(fs.existsSync(this.path)){
+      const products = JSON.parse(fs.readFileSync(this.path),'utf-8')
+      this.products = products
+    }else{
+      this.products = []
+    }
+  }
 }
 
-const productManager = new ProductManager()
+const productManager = new ProductManager('./product.json')
 //Testing
 console.log(productManager.getPruduct())
-
 productManager.addProduct('producto prueba','Este es un prodcuto prueba',200,'Sin Imagne','abc123',25)
-
 console.log(productManager.getPruduct())
-
-productManager.addProduct('producto prueba','Este es un prodcuto prueba',200,'Sin Imagne','abc123',25)
-
-console.log(productManager.getProductByID(5))
-console.log(productManager.getProductByID(1))
-
+console.log(productManager.getProductByID(2)) //buscamos un preducto que no existe
+console.log(productManager.getProductByID(1)) //buscamos un preducto que existe
+productManager.updateProduct(1,'producto actualizado','este es un producto actulizado',200,'sin imagen','asdd','30')
+productManager.deleteProduct(1)
 
 
 
 
 
 
-// productManager.addProduct('producto2','prodcuto22p',50,'/eee/ddd','a2',20)
-// productManager.addProduct('producto3','prodcuto333p',60,'/fff/ddd','a3',30)
-// productManager.addProduct('producto4','prodcuto444p',70,'/ggg/ddd','a4',40)
-
-// //muestro productos cargados
-// console.log(productManager.getPruduct())
-
-// //error de carga de producto faltan campos
-// productManager.addProduct('prodcuto444p,/ggg/ddd','a4',40)
-
-// //error 
