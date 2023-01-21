50cb5f11eb5d0cf30268a541152cc8b308db25ec
espesot <enzospesot@hotmail.com>
2023-01-21 19:15:48 -0300

    desfio complementario.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { Server } from 'socket.io'
-import routes from './routes/index.routes.js'
-import {engine} from 'express-handlebars'
-import { webSocketInit } from './utils/websocket.js'
 import dotenv from 'dotenv'
+dotenv.config()
+import {engine} from 'express-handlebars'
+import { Server } from 'socket.io'
+import { webSocketInit } from './utils/websocket.js'
+import routes from './routes/index.routes.js'
 import dbConnect from './configs/db.config.js'
 
-dotenv.config()   // agregado archivo para tomar variables de congig 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -15,7 +15,7 @@ app.use(express.static('public/'))
 
 app.engine('handlebars', engine())
 app.set('view engine','handlebars')
-app.set('view','src/views')
+app.set('views','src/views')
 
 app.use((req,res,next)=>{
   req.io = io
@@ -27,7 +27,7 @@ dbConnect()
 
 app.use(routes)
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () => console.log(`🚀 Server started on port http://localhost:${PORT}`))
 server.on('error', (err) => console.log(err))
 
